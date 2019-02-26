c259727dd5bbe0dae8836ad0ac9b648e4e4dd56d
gitJank <justinjankiewicz@gmail.com>
2019-02-25 21:47:58 -0600

    feat: added abbility to create new gifts

---
+++ giftgiver/src/components/Gift.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Button
+} from "react-bootstrap";
+
+class Gift extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      person: "",
+      present: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Form>
+          <FormGroup>
+            <FormLabel>Person</FormLabel>
+            <FormControl
+              className="input-person"
+              onChange={e => this.setState({ person: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel>Present</FormLabel>
+            <FormControl
+              className="input-present"
+              onChange={e => this.setState({ present: e.target.value })}
+            />
+          </FormGroup>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default Gift;
